446d6394cb6198fd1f8709b38527585f0b91eb16
Genrih <genrih171@rambler.ru>
2023-07-06 14:09:20 +0400

    fix: toogle filters

--- js/filters.js
+++ js/filters.js
@@ -1,12 +1,9 @@
-import { createRandomIntegerFromRangeNoRepeats, debounce } from './util.js';
+import { createRandomIntegerFromRangeNoRepeats } from './util.js';
 
 const AMOUNT_RANDOM_PHOTOS = 10;
 
 const filters = document.querySelector('.img-filters');
 const filterButtons = filters.querySelectorAll('.img-filters__button');
-const buttonRandomFilter = filters.querySelector('#filter-random');
-const buttonDefaultFilter = filters.querySelector('#filter-default');
-const buttonRaitingFilter = filters.querySelector('#filter-discussed');
 
 filters.classList.remove('img-filters--inactive');
 
@@ -15,20 +12,18 @@ const removeThumbnails = () => {
   thumbnails.forEach((el) => el.remove());
 };
 
-const changeButtonClass = (button) => {
+const changeButtonClass = (evt) => {
   filterButtons.forEach((el) => el.classList.remove('img-filters__button--active'));
-  button.classList.add('img-filters__button--active');
+  evt.target.classList.add('img-filters__button--active');
 };
 
 const addFilters = (photos, cb) => {
   const toggleDefaultFilter = () => {
-    changeButtonClass(buttonDefaultFilter);
     removeThumbnails();
     cb(photos);
   };
 
   const toggleRandomFilter = () => {
-    changeButtonClass(buttonRandomFilter);
     removeThumbnails();
     const randomOrder = [];
     const randomPhotoNumber = createRandomIntegerFromRangeNoRepeats(0, photos.length - 1);
@@ -39,16 +34,38 @@ const addFilters = (photos, cb) => {
   };
 
   const toggleRaitingFilter = () => {
-    changeButtonClass(buttonRaitingFilter);
     removeThumbnails();
     const sortedPhotos = photos.slice();
     sortedPhotos.sort((a, b) => b.likes - a.likes);
     cb(sortedPhotos);
   };
 
-  buttonDefaultFilter.addEventListener('click', debounce(toggleDefaultFilter));
-  buttonRandomFilter.addEventListener('click', debounce(toggleRandomFilter));
-  buttonRaitingFilter.addEventListener('click', debounce(toggleRaitingFilter));
+  const toogleFilter = (evt) => {
+    switch (evt.target.closest('.img-filters__button').id) {
+      case 'filter-default':
+        toggleDefaultFilter();
+        break;
+      case 'filter-random':
+        toggleRandomFilter();
+        break;
+      case 'filter-discussed':
+        toggleRaitingFilter();
+        break;
+      default:
+        break;
+    }
+  };
+
+  const onFilterButtons = (callback, timeoutDelay = 500) => {
+    let timeoutId;
+    return (evt) => {
+      changeButtonClass(evt);
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => callback(evt), timeoutDelay);
+    };
+  };
+
+  filters.addEventListener('click', onFilterButtons(toogleFilter));
 };
 
 
